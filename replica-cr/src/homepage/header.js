83af188ce5e817ecf8c51a5cc5157e7f29823930
Neeharika gupta <neeharika2503@gmail.com>
2023-08-24 16:55:44 +0530

    commit 2

--- replica-cr/src/homepage/header.js
+++ replica-cr/src/homepage/header.js
@@ -4,12 +4,15 @@ import logo from './logo.svg';
 
 function Header() {
   return (
-    <div classname='header'>
+    <div classname="header">
         <img src={logo} className="header_logo" alt="logo" />
-    <div classname='headerlogin'>
-        <span classname='login'>Hello guest!</span>
-    </div>
+        <div className="header__nav">
+            <div className="header__option">
+                <span className="header__optionLineOne">Hello Guest</span>
+                <span className="header__optionLineTwo">Sign In</span>
+            </div>
         </div>
+    </div>  
   )
 }
 
